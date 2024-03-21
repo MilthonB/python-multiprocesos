b9cc3bb1ede0639e56f2b031b35cc558d1ade9f8
Milthon B <81788252+MilthonB@users.noreply.github.com>
2024-03-21 11:32:41 -0700

    Add function ftps lsiten to app local

---
+++ FTPS/ftps_server.py
@@ -0,0 +1,27 @@
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+from pathlib import Path
+
+def main():
+    autorizer =  DummyAuthorizer()
+    
+    autorizer.add_user(username='uranaibaba',password='roshi',homedir='C:/TEST-FTP',msg_login='Login successful',msg_quit='Goodbye', perm='elradfmwMT')
+    
+    handler = FTPHandler
+    handler.authorizer =  autorizer
+    
+    handler.banner = ' pyftpdlib based ftpd ready '
+    
+    addres = ('127.0.0.1', 2121)
+    server = FTPServer( addres, handler=handler )
+    
+    server.max_cons =256
+    server.max_cons_per_ip = 5
+    
+    server.serve_forever()
+    
+    
+if __name__ == '__main__':
+    main()
+    
